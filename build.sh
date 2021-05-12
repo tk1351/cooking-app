55d87d634315acbfe94ae85e40a514427203b0bc
tk1351 <takakikitsui0111@gmail.com>
2021-05-12 13:36:21 +0900

    Nest.jsの起動確認

---
+++ build.sh
@@ -0,0 +1,3 @@
+# development環境でdocker build
+# $ ./build.sh
+docker-compose -f docker-compose.dev.yml up --build -d
---
+++ execApi.sh
@@ -0,0 +1,3 @@
+# apiコンテナに接続する
+# $ ./execApi.sh
+docker-compose -f docker-compose.dev.yml exec api bash
