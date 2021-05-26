9a4d22b65bc068ea5d5f1b21fb21fa0c080c7743
tk1351 <takakikitsui0111@gmail.com>
2021-05-27 08:44:55 +0900

    material-uiインストール

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,27 @@
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ThemeProvider as MaterialUIThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../src/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  )
 }
 export default MyApp
