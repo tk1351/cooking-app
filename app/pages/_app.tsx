b607c15182dc3987dd24794e2f7ddd8a35521b29
tk1351 <takakikitsui0111@gmail.com>
2021-06-17 11:03:33 +0900

    Material-UIのSSR時に出るエラーの修正

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  ThemeProvider as MaterialUIThemeProvider,
-  StylesProvider,
-} from '@material-ui/styles'
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
+import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles'
+import { StylesProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { unwrapResult } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
@@ -27,20 +26,22 @@ const AppInit = () => {
   return null
 }
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles)
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles)
     }
   }, [])
   return (
     <Provider store={store}>
       <StylesProvider injectFirst>
         <MaterialUIThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-          <AppInit />
+          <StyledComponentsThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+            <AppInit />
+          </StyledComponentsThemeProvider>
         </MaterialUIThemeProvider>
       </StylesProvider>
     </Provider>
