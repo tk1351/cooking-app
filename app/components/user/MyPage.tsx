7e54582b95dab48009ff23e7616371ef396e8343
tk1351 <takakikitsui0111@gmail.com>
2021-07-07 07:29:33 +0900

    マイページのCSS適用

--- app/components/user/MyPage.tsx
+++ app/components/user/MyPage.tsx
@@ -1,8 +1,9 @@
 import React, { VFC } from 'react'
 import Link from 'next/link'
-import { Typography, Button } from '@material-ui/core'
+import { Typography, Button, Grid } from '@material-ui/core'
 import { IUser } from '../../re-ducks/auth/type'
 import Alert from '../common/Alert'
+import styles from '../../styles/components/user/mypage.module.css'
 
 type Props = {
   user: IUser
@@ -10,22 +11,39 @@ type Props = {
 
 const MyPage: VFC<Props> = ({ user }) => {
   return (
-    <div>
+    <Grid container className={styles.wrapper}>
       <Alert />
-      <h1>マイページ</h1>
-      <Typography gutterBottom variant="h5" component="h2">
-        ユーザー名:{user.name}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-        好きな食べ物:{user.favoriteDish}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-        得意料理:{user.specialDish}
-      </Typography>
-      <Button size="small" color="primary" type="button" variant="contained">
-        <Link href={`${user.id}/edit`}>プロフィール編集</Link>
-      </Button>
-    </div>
+      <Grid item xs={12}>
+        <Grid container justify="center">
+          <h1 className={styles.h1}>マイページ</h1>
+        </Grid>
+        <Grid container>
+          <Typography gutterBottom variant="h5" component="h2">
+            ユーザー名:{user.name}
+          </Typography>
+        </Grid>
+        <Grid container>
+          <Typography gutterBottom variant="h5" component="h2">
+            好きな食べ物:{user.favoriteDish}
+          </Typography>
+        </Grid>
+        <Grid container>
+          <Typography gutterBottom variant="h5" component="h2">
+            得意料理:{user.specialDish}
+          </Typography>
+        </Grid>
+        <Grid container justify="center">
+          <Button
+            size="small"
+            color="primary"
+            type="button"
+            variant="contained"
+          >
+            <Link href={`${user.id}/edit`}>プロフィール編集</Link>
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
