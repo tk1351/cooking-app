d8c4a34a99aa27857a604e38205c5f9d695dee7b
tk1351 <takakikitsui0111@gmail.com>
2021-07-07 07:28:00 +0900

    ユーザーリストアイテムページCSS適用

--- app/components/admin/UsersList.tsx
+++ app/components/admin/UsersList.tsx
@@ -1,12 +1,13 @@
 import React, { VFC, useState } from 'react'
 import { useRouter } from 'next/router'
-import { Grid, Typography, Button } from '@material-ui/core'
+import { Grid, Typography, Button, Container } from '@material-ui/core'
 import InfiniteScroll from 'react-infinite-scroller'
 import { IUser } from '../../re-ducks/auth/type'
 import { useAppSelector } from '../../re-ducks/hooks'
 import { selectUserId } from '../../re-ducks/auth/authSlice'
 import UserListItem from './UserListItem'
 import Spinner from '../common/Spinner'
+import styles from '../../styles/components/admin/usersList.module.css'
 
 import API from '../../src/utils/api'
 
@@ -37,27 +38,25 @@ const UsersList: VFC<Props> = ({ users }) => {
 
   const loader = <Spinner key={0} />
   return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6">ユーザーリスト</Typography>
-          <div>
-            <InfiniteScroll
-              loadMore={loadMore}
-              hasMore={hasMore}
-              loader={loader}
-            >
-              {items.map((item) => (
-                <UserListItem key={item.id} user={item} />
-              ))}
-            </InfiniteScroll>
-          </div>
-        </Grid>
+    <Container component="main" maxWidth={false} className={styles.container}>
+      <Grid container justify="center" className={styles.h1}>
+        <h1>ユーザーリスト</h1>
       </Grid>
-      <Button onClick={() => router.push(`/admin/${Number(adminUserId)}`)}>
-        戻る
-      </Button>
-    </>
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} loader={loader}>
+        {items.map((item) => (
+          <UserListItem key={item.id} user={item} />
+        ))}
+      </InfiniteScroll>
+      <Grid container>
+        <Button
+          variant="contained"
+          color="inherit"
+          onClick={() => router.push(`/admin/${Number(adminUserId)}`)}
+        >
+          戻る
+        </Button>
+      </Grid>
+    </Container>
   )
 }
 
