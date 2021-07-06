e9810f89a98de27475adb5bdd850b49416e7cb2c
tk1351 <takakikitsui0111@gmail.com>
2021-07-07 07:27:04 +0900

    ユーザー情報詳細ページCSS適用

--- app/components/admin/UserInfo.tsx
+++ app/components/admin/UserInfo.tsx
@@ -1,9 +1,10 @@
 import React, { VFC } from 'react'
 import { useRouter } from 'next/router'
 import { IUser } from '../../re-ducks/auth/type'
-import { Button, Typography } from '@material-ui/core'
+import { Button, Typography, Grid } from '@material-ui/core'
 import { useAppDispatch } from '../../re-ducks/hooks'
 import { deleteUserWithAdminPriviledge } from '../../re-ducks/auth/authSlice'
+import styles from '../../styles/components/admin/userInfo.module.css'
 
 type Props = {
   user: IUser
@@ -23,29 +24,43 @@ const UserInfo: VFC<Props> = ({ user }) => {
   }
 
   return (
-    <div>
-      <h1>{user.name}さん</h1>
-      <Typography gutterBottom variant="h5" component="h2">
-        好きな食べ物:{user.favoriteDish}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="h2">
-        得意料理:{user.specialDish}
-      </Typography>
-      {user.role === 'user' ? (
-        <Button
-          color="secondary"
-          variant="contained"
-          onClick={onDeleteUserClicked}
-        >
-          ユーザーを削除する
-        </Button>
-      ) : (
-        <></>
-      )}
-      <Button onClick={() => router.push('/admin/users/list')}>
-        ユーザーリストへ戻る
-      </Button>
-    </div>
+    <Grid container className={styles.wrapper}>
+      <Grid item xs={12}>
+        <Grid container justify="center">
+          <h1 className={styles.h1}>{user.name}さん</h1>
+        </Grid>
+        <Grid container>
+          <Typography gutterBottom variant="h5" component="h2">
+            好きな食べ物:{user.favoriteDish}
+          </Typography>
+        </Grid>
+        <Grid container>
+          <Typography gutterBottom variant="h5" component="h2">
+            得意料理:{user.specialDish}
+          </Typography>
+        </Grid>
+        <Grid container className={styles.buttonWrapper}>
+          <Button
+            variant="contained"
+            color="inherit"
+            onClick={() => router.push('/admin/users/list')}
+          >
+            ユーザーリストへ戻る
+          </Button>
+          {user.role === 'user' ? (
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={onDeleteUserClicked}
+            >
+              ユーザーを削除する
+            </Button>
+          ) : (
+            <></>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
