0addf93874cd660c4863d627aee50f6f3854dd06
tk1351 <takakikitsui0111@gmail.com>
2021-07-15 09:55:36 +0900

    Registerコンポーネントの削除

--- app/pages/register.tsx
+++ app/pages/register.tsx
@@ -1,13 +1,11 @@
 import React, { VFC } from 'react'
 import Navbar from '../components/common/Navbar'
-import Register from '../components/auth/Register'
 import Footer from '../components/common/Footer'
 
 const register: VFC = () => {
   return (
     <>
       <Navbar />
-      <Register />
       <Footer />
     </>
   )
