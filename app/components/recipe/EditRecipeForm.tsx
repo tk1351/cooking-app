03d8db51db10c2aa75ab074d9e2cb27c8a2cd8d9
tk1351 <takakikitsui0111@gmail.com>
2021-07-15 09:52:44 +0900

    hot fix

--- app/components/recipe/EditRecipeForm.tsx
+++ app/components/recipe/EditRecipeForm.tsx
@@ -395,7 +395,7 @@ const EditRecipeForm: VFC<Props> = ({ recipe }) => {
                 <Controller
                   name={`recipeDescriptions.${index}.order`}
                   control={control}
-                  defaultValue={field.order}
+                  defaultValue={Number(field.order)}
                   render={({
                     field: { onChange, ref },
                     formState: { errors },
