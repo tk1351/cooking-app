8beb1a4e329528d1b687c45931986b7f7288b9b2
tk1351 <takakikitsui0111@gmail.com>
2021-07-21 16:53:25 +0900

    SSG対応からuseEffectでuserをfetchするのに変更

--- app/pages/profile.tsx
+++ app/pages/profile.tsx
@@ -1,31 +1,16 @@
 import React, { VFC } from 'react'
-import { InferGetStaticPropsType } from 'next'
-import axios from 'axios'
 import Navbar from '../components/common/Navbar'
 import Footer from '../components/common/Footer'
-import { IUser } from '../re-ducks/auth/type'
 import AuthorProfile from '../components/common/AuthorProfile'
 
-const profile: VFC<Props> = (props) => {
-  console.log('data', props)
+const profile: VFC = () => {
   return (
     <div>
       <Navbar />
-      <AuthorProfile user={props} />
+      <AuthorProfile />
       <Footer />
     </div>
   )
 }
-
-export const getStaticProps = async () => {
-  const id = 1
-  const url = `${process.env.API_URL}/users/author/${id}`
-  const res = await axios.get<IUser>(url)
-  return {
-    props: res.data,
-  }
-}
-
-type Props = InferGetStaticPropsType<typeof getStaticProps>
 
 export default profile
